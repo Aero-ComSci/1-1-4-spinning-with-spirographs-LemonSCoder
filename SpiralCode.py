72a974f75351dbe075043d5d1ae8bfd1dab6e630
Sophie Le <66530939+LemonSCoder@users.noreply.github.com>
2024-09-09 23:05:49 -0700

    Update SpiralCode.py
    Adjusted distances each concentric square has from each other as well as what determines the background color of the turtle program's output.

--- SpiralCode.py
+++ SpiralCode.py
@@ -1,30 +1,26 @@
 import math
 import random
 import turtle
+background_colors = ["white"]
 colors = ["light slate gray", "alice blue", "light steel blue", "cornflower blue", "royal blue", "navy", "midnight blue", "light blue", "powder blue", "light sky blue", "azure", "light cyan", "pale turquoise","turquoise", "aquamarine"]
 spiral_turtle = turtle.Turtle()
 spiral_turtle.speed(20)
-turtle.screensize(800, 800, random.choice(colors))
-concentric_number = 120
-while concentric_number <= 600:
+turtle.screensize(800, 800, random.choice(background_colors))
+previous_concentric_number = 0
+concentric_number = 80
+while concentric_number <= 400:
     spiral_turtle.pencolor(random.choice(colors))
     spiral_turtle.pensize(15)
     spiral_turtle.penup()
-    spiral_turtle.goto(0, 0)
-    spiral_turtle.right(90)
-    spiral_turtle.forward(concentric_number)
-    spiral_turtle.pendown()
-    spiral_turtle.right(90)
-    spiral_turtle.penup()
-    spiral_turtle.goto(concentric_number, 0)
+    spiral_turtle.goto(0, (concentric_number)/(2))
+    spiral_turtle.forward(-(concentric_number))
     spiral_turtle.pendown()
     turn_count = 0
     while turn_count < 4:
         spiral_turtle.forward(concentric_number)
         spiral_turtle.right(90)
         turn_count += 1
-    concentric_number += 120
-
-
-
-
+    previous_concentric_number += 80
+    concentric_number += 80
+    print(concentric_number)
+turtle.done()
