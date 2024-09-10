21c8846c49dd4c42f5e2a98f64f522491ff27e29
Sophie Le <66530939+LemonSCoder@users.noreply.github.com>
2024-09-10 14:16:56 -0700

    Update SpiralCode.py

--- SpiralCode.py
+++ SpiralCode.py
@@ -8,11 +8,11 @@ spiral_turtle.speed(20)
 turtle.screensize(800, 800, random.choice(background_colors))
 previous_concentric_number = 0
 concentric_number = 80
-while concentric_number <= 400:
+while concentric_number <= (80 * 20):
     spiral_turtle.pencolor(random.choice(colors))
     spiral_turtle.pensize(15)
     spiral_turtle.penup()
-    spiral_turtle.goto(0, (concentric_number)/(2))
+    spiral_turtle.goto((concentric_number / 2), ((concentric_number) / 2))
     spiral_turtle.forward(-(concentric_number))
     spiral_turtle.pendown()
     turn_count = 0
@@ -22,5 +22,4 @@ while concentric_number <= 400:
         turn_count += 1
     previous_concentric_number += 80
     concentric_number += 80
-    print(concentric_number)
 turtle.done()
