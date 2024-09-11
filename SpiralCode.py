f965f32a57631629b9ad633efcea41f75800969c
Sophie Le <66530939+LemonSCoder@users.noreply.github.com>
2024-09-11 00:31:59 -0700

    Update SpiralCode.py
    Removed previous_concentric_number (wasn't necessary for the program)

--- SpiralCode.py
+++ SpiralCode.py
@@ -6,9 +6,8 @@ colors = ["light slate gray", "alice blue", "light steel blue", "cornflower blue
 spiral_turtle = turtle.Turtle()
 spiral_turtle.speed(20)
 turtle.screensize(800, 800, random.choice(background_colors))
-previous_concentric_number = 0
 concentric_number = 80
-while concentric_number <= (80 * 20):
+while concentric_number <= (80 * 15):
     spiral_turtle.pencolor(random.choice(colors))
     spiral_turtle.pensize(15)
     spiral_turtle.penup()
@@ -20,6 +19,5 @@ while concentric_number <= (80 * 20):
         spiral_turtle.forward(concentric_number)
         spiral_turtle.right(90)
         turn_count += 1
-    previous_concentric_number += 80
     concentric_number += 80
 turtle.done()
