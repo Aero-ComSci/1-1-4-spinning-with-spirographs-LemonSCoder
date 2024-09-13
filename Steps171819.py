ee144c0df5daa0bc55c1bd2671bcb8de5d1d03dd
Sophie Le <66530939+LemonSCoder@users.noreply.github.com>
2024-09-13 13:47:00 -0700

    Update Steps171819.py
    Added Comments.

--- Steps171819.py
+++ Steps171819.py
@@ -1,6 +1,6 @@
 #Answer to Step Seventeen.
 import turtle as trtl
-
+#Makes sure pen goes to the edge of the screen.
 painter = trtl.Turtle()
 painter.penup()
 painter.goto(-200, 0)
@@ -10,18 +10,25 @@ x = -200
 y = 0
 move_x = 1
 move_y = 1
+#Runs while x is less than or equal to zero.
 while (x <= 0):
-
+  #Runs while y is less than one hundred
   while (y < 100):
+    #x moves right
     x = x + move_x
+    #y moves up
     y = y + move_y
     painter.goto(x,y)
+  #y moves down next time
   move_y = -1
-  
+  #Runs while y is greater than zero.
   while (y > 0):
+    #x moves right
     x = x + move_x
+    #y moves down
     y = y + move_y
     painter.goto(x,y)
+  #y goes up next time
   move_y = 1
   
 wn = trtl.Screen()
@@ -29,7 +36,7 @@ wn.mainloop()
 
 #Solution to Question Eighteen.
 import turtle as trtl
-
+#Makes sure pen goes to the edge of the screen.
 painter = trtl.Turtle()
 painter.penup()
 painter.goto(-200, 0)
@@ -39,26 +46,36 @@ x = -200
 y = 0
 move_x = 1
 move_y = 1
+#Runs while x is less than zero.
 while (x < 0):
-
+  #Runs while y is less than one hundred.
   while (y < 100):
+    #x moves to the right
     x = x + move_x
+    #y moves up
     y = y + move_y
     painter.goto(x,y)
+  #y moves down next time
   move_y = -1
-  
+  #Runs while y is greater than negative one hundred.
   while (y > (-100)):
+    #x moves to the right
     x = x + move_x
+    #y moves down
     y = y + move_y
     painter.goto(x,y)
+  #y moves up next time
   move_y = 1
-
+  #Runs while y is less than one hundred.
   while (y < (100)):
+    #x moves to the right
     x = x + move_x
+    #y moves up
     y = y + move_y
     painter.goto(x,y)
+  #y moves up next time
   move_y = 1
-  
+#Makes sure pen goes to the edge of the screen.
 painter.penup()
 painter.goto(-200, 0)
 painter.pendown()
@@ -67,22 +84,31 @@ x = -200
 y = 0
 move_x = 1
 move_y = 1
+#Runs while x is less than or equal to zero
 while (x <= 0):
-  
+  #Runs while x is less than negative one hundred
   while (x < (-100)):
+    #x moves right
     x = x + move_x
+    #y moves down
     y = y - move_y
     painter.goto(x,y)
+  #y moves up next time
   move_y = 1
-
+  #Runs while y is less than one hundred
   while (y < 100):
+    #x moves right
     x = x + move_x
+    #y moves up
     y = y + move_y
     painter.goto(x,y)
+  #y moves up next time
   move_y = 1
-  
+  #Runs while x is less than four hundred
   while (x < 400):
+    #x moves right
     x = x + move_x
+    #y moves down
     y = y - move_y
     painter.goto(x,y)
 
@@ -93,36 +119,47 @@ wn.mainloop()
 import turtle as trtl
 
 painter = trtl.Turtle()
-
+#Will run infinitely since nothing equates this while loop to false and there is no break in the loop.
 while True:
   x = -200
   y = 0
   move_x = 1
   move_y = 1
+  #Runs while x is less than zero (will happen continuously since x resets to being equal to negative two hundred every time the loop runs again (infinitely)
   while (x < 0):
-  
+    #Makes sure pen goes to the edge of the screen.
     painter.penup()
     painter.goto(-200, 0)
     painter.pendown()
-
+    #Runs while y is less than one hundred.
     while (y < 100):
+      #x moves right
       x = x + move_x
+      #y moves up
       y = y + move_y
       painter.goto(x,y)
+    #y moves down next time
     move_y = -1
-  
+    #Runs while y is greater than negative one hundred
     while (y > (-100)):
+      #x moves right
       x = x + move_x
+      #y moves up
       y = y + move_y
       painter.goto(x,y)
+    #y moves up next time
     move_y = 1
-
+    #Runs while y is less than one hundred.
     while (y < (100)):
+      #x moves right
       x = x + move_x
+      #y goes up
       y = y + move_y
       painter.goto(x,y)
+    #y moves up next time
     move_y = 1
-  
+    
+  #Makes sure pen goes to the edge of the screen.
   painter.penup()
   painter.goto(-200, 0)
   painter.pendown()
@@ -131,24 +168,34 @@ while True:
   y = 0
   move_x = 1
   move_y = 1
+  #Runs while x is less than zero.
   while (x < 0):
-  
+    #Runs while x is less than negative one hundred.
     while (x < (-100)):
+      #x moves right
       x = x + move_x
+      #y moves down
       y = y - move_y
       painter.goto(x,y)
+    #y moves up next time
     move_y = 1
-
+    #Runs while y is less than one hundred
     while (y < 100):
+      #x moves right
       x = x + move_x
+      #y moves up
       y = y + move_y
       painter.goto(x,y)
+    #y moves up next time
     move_y = 1
-  
+    #Runs while x is less than four hundred
     while (x < 400):
+      #x moves right
       x = x + move_x
+      #y moves down
       y = y - move_y
       painter.goto(x,y)
+    #y moves down nex time
     move_y = -1
 
 
